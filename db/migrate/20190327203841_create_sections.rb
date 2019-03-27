e0dfbfaad7fc91d4a53006197058f6c3f54e702f
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-03-27 14:15:16 -0700

    deploy to heroku

--- db/migrate/20190327203841_create_sections.rb
+++ db/migrate/20190327203841_create_sections.rb
@@ -1,7 +1,9 @@
 class CreateSections < ActiveRecord::Migration[5.2]
   def change
     create_table :sections do |t|
-
+      t.string :name
+      t.integer :position
+      t.boolean :visible, default: false
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+subjects = [
+  {name: "Ruby", position: 1, visible: true},
+  {name: "Ruby on Rails", position: 2, visible: false},
+  {name: "CoffeeScript", position: 3, visible: true},
+  {name: "Python", position: 4, visible: false},
+  {name: "JavaScript", position: 5, visible: true},
+]
